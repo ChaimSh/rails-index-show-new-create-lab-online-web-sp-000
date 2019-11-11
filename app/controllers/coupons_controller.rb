5aba2ccea42ce47f9f8bd9a8bb6685f00916a976
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-11-11 02:45:53 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -15,7 +15,8 @@ class CouponsController < ApplicationController
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
-    @coupoin.save
+    @coupon.save
+    redirect_to coupon_path(coupon)
   end
 
 
